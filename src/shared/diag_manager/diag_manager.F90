0131708571b0c267cc0a87c81109a8e68f34d44f
Chris Bladwell <c.bladwell@unsw.edu.au>
2020-12-09 15:32:51 +1100

    initial code added for exact hat averaging

--- src/shared/diag_manager/diag_manager.F90
+++ src/shared/diag_manager/diag_manager.F90
@@ -1442,7 +1442,7 @@ CONTAINS
 #endif
     LOGICAL :: average, phys_window, need_compute
     LOGICAL :: reduced_k_range, local_output
-    LOGICAL :: time_max, time_min, time_rms
+    LOGICAL :: time_max, time_min, time_rms, fwd_risavg
     LOGICAL :: missvalue_present
     LOGICAL, ALLOCATABLE, DIMENSION(:,:,:) :: oor_mask
     CHARACTER(len=256) :: err_msg_local
@@ -1688,6 +1688,12 @@ CONTAINS
        IF ( need_compute ) THEN
           l_start = output_fields(out_num)%output_grid%l_start_indx
           l_end = output_fields(out_num)%output_grid%l_end_indx
+       END IF
+
+       ! compute the forward rising average weight from 0 to 1
+       fwd_risavg = output_fields(out_num)%fwd_risavg
+       IF (fwd_risavg) THEN
+          weight1 = time-output_fields(out_num)%last_output
        END IF
 
        ! compute the diurnal index
@@ -2948,7 +2954,7 @@ CONTAINS
 
     TYPE(time_type) :: middle_time
     LOGICAL :: time_max, time_min, reduced_k_range, missvalue_present
-    LOGICAL :: average, time_rms, need_compute, phys_window
+    LOGICAL :: average, time_rms, need_compute, phys_window, fwd_risavg
     INTEGER :: in_num, file_num, freq, units
     INTEGER :: b1,b2,b3,b4 ! size of buffer along x,y,z,and diurnal axes
     INTEGER :: i, j, k, m
@@ -2976,6 +2982,7 @@ CONTAINS
     file_num = output_fields(out_num)%output_file
     freq = files(file_num)%output_freq
     units = files(file_num)%output_units
+    fwd_risavg = output_fields(out_num)%fwd_risavg
 
     ! If average get size: Average intervals are last_output, next_output
     IF ( average ) THEN
